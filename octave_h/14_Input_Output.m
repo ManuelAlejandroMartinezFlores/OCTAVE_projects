1491c86c17347abea6a3f456a63d1e0a2354cc75
Manuel <manuelalejandromartinezf@gmail.com>
2022-06-25 11:46:08 -0600

    Input Output

---
+++ octave_h/14_Input_Output.m
@@ -0,0 +1,15 @@
+% Input Output
+
+input("num\n")
+
+input("str\n", 's')
+
+menu("Menu", "Option1", "Option2") 
+
+% Files
+
+A = [1:3; 4:6; 7:9];
+save "14_MyFile.m" A
+
+load "14_MyFile.m" 
+A
---
+++ octave_h/14_MyFile.m
@@ -0,0 +1,10 @@
+# Created by Octave 7.1.0, Sat Jun 25 11:38:43 2022 CST <manuelmartinezflores@MacBook-Pro-de-Manuel.local>
+# name: A
+# type: matrix
+# rows: 3
+# columns: 3
+ 1 2 3
+ 4 5 6
+ 7 8 9
+
+

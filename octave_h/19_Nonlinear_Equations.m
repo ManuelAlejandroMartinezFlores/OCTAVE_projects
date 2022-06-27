e4fbc126f4dc4d0d2bddd8499a7c3cf6d9a281b9
Manuel <manuelalejandromartinezf@gmail.com>
2022-06-26 18:33:23 -0600

    Nonlinear Equations

---
+++ octave_h/19_Nonlinear_Equations.m
@@ -0,0 +1,17 @@
+% Nonlinear Equations
+
+function y = f(x)
+    y(1) = -2 * x(1)^2 + 3 * x(1) * x(2) + 4 * sin(x(2)) - 6;
+    y(2) = 3 * x(1)^2 - 2 * x(1) * x(2)^2 + 3 * cos(x(1)) + 4;
+endfunction
+
+[x, fval, info] = fsolve(@f, [1; 2])
+
+function J = jacobian(x)
+    J(1, 1) = 3 * x(2) - 4 * x(1);
+    J(1, 2) = 4 * cos(x(2)) + 3 * x(1);
+    J(2, 1) = -2 * x(2)^2 - 3 * sin(x(1)) + 6 * x(1);
+    J(2, 2) = -4 * x(1) * x(2);
+endfunction
+
+[x, fval, info] = fsolve({@f, @jacobian}, [1; 2])
